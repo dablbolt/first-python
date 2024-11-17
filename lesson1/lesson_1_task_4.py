546594a1269f623013f7ef60263a88e9072cd139
dablbolt <dablbolt@mail.ru>
2024-11-18 01:06:25 +0500

    Домашнка 1

---
+++ lesson1/lesson_1_task_4.py
@@ -0,0 +1,3 @@
+def print_greeting(): #Создание переменной
+    print('Привет,мир!')
+print_greeting()
---
+++ lesson1/lesson_1_task_5.py
@@ -0,0 +1,16 @@
+def tt(let):
+    print(let, end='') #Параметризация функций
+tt(8)
+tt(8)
+tt(0)
+tt(0)
+tt(5)
+tt(5)
+tt(5)
+tt(3)
+tt(5)
+tt(3)
+tt(5)
+
+
+
